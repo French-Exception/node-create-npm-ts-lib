1839898e129670a37ce3b186b3a10e40f19aa8ab
Stephane ERARD <serard06@protonmail.com>
2019-12-14 02:47:07 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import * as lib from "@frenchex/create-npm-lib-lib";
+import * as path from "path";
+
+const logger = require('@log4js-node/log4js-api').getLogger();
+
+const npmCreate = new lib.NpmCreate(logger);
+
+(async () => {
+    await npmCreate.create({
+        gitBin: process.env.git_bin || await lib.which('git'),
+        npmBin: process.env.npm_bin || await lib.which('npm'),
+        path: process.env.working_directory || process.cwd(),
+        branch: process.env.branch || 'develop',
+        name: process.env.name || path.basename(process.cwd()),
+        packageJson: {
+            main: 'src/index.js',
+            homepage: "",
+            repository: {
+                type: "git",
+                url: ""
+            },
+            scripts: [
+                {name: 'dev', script: 'tsc -w'},
+                {name: 'build', script: 'tsc'},
+            ],
+            publishConfig: {
+                "access": true
+            },
+            devDependencies: [
+                {name: 'typescript', version: {prefix: '*'}},
+                {name: 'ts-node', version: {prefix: '*'}},
+                {scope: 'types', name: 'node', version: {prefix: '*'}},
+                {scope: 'types', name: 'mocha', version: {prefix: '*'}},
+                {scope: 'types', name: 'chai', version: {prefix: '*'}},
+                {name: 'mocha', version: {prefix: '*'}},
+                {name: 'chai', version: {prefix: '*'}},
+                {name: 'release-it', version: {prefix: '*'}},
+            ]
+        },
+        scope: process.env.scope || path.basename(path.join(process.cwd(), '..')),
+        version: {major: 0, minor: 0, patch: 1}
+    });
+})();
